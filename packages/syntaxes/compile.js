212d603cb691e0dcf9cbf918b6ac2117214116a3
nkomiya <n.comiya@gmail.com>
2022-09-25 23:24:38 +0900

    chore: modify compile script

--- packages/syntaxes/compile.js
+++ packages/syntaxes/compile.js
@@ -1,13 +1,24 @@
-const glob = require("glob");
-const yaml = require("js-yaml");
 const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
+const yaml = require("js-yaml");
+
+const OUT = path.join(__dirname, "../../dist/syntaxes");
 
 function compile(dirname) {
+  const outDir = path.join(OUT, dirname);
+  fs.mkdirSync(outDir, { recursive: true });
+
+  fs.copyFileSync(
+    path.join(__dirname, dirname, "config.json"),
+    path.join(outDir, "config.json")
+  );
+
   glob(`${dirname}/*.yaml`, (e, files) => {
     if (e) throw e;
     files.forEach((f) => {
       const data = JSON.stringify(yaml.load(fs.readFileSync(f, "utf-8")));
-      const out = f.replace(/yaml$/, "json");
+      const out = path.join(outDir, path.basename(f).replace(/yaml$/, "json"));
       fs.writeFileSync(out, data);
     });
   });
