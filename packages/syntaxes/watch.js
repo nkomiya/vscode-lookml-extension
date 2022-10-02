1f77888b466969a2e5adf327946015728fc760c7
nkomiya <n.comiya@gmail.com>
2022-10-02 17:21:12 +0900

    feat: add liquid support

--- packages/syntaxes/watch.js
+++ packages/syntaxes/watch.js
@@ -16,6 +16,22 @@ function getOutput(file) {
   return path.join(OUT, path.relative(ROOT, file)).replace(/yaml$/, "json");
 }
 
+function now() {
+  const now = new Date();
+  const date = [
+    now.getFullYear(),
+    `0${now.getMonth() + 1}`.slice(-2),
+    `0${now.getDate()}`.slice(-2),
+  ].join("-");
+  const time = [
+    `0${now.getHours()}`.slice(-2),
+    `0${now.getMinutes()}`.slice(-2),
+    `0${now.getSeconds()}`.slice(-2),
+  ].join(":");
+  const ms = `00${now.getMilliseconds()}`.slice(-3);
+  return date + " " + time + "." + ms;
+}
+
 function onChange(file) {
   const output = getOutput(file);
   if (output == undefined || lock[output]) return;
@@ -28,13 +44,15 @@ function onChange(file) {
     try {
       const data = JSON.stringify(yaml.load(fs.readFileSync(file, "utf-8")));
       fs.writeFileSync(output, data);
-      console.log(`${output} is updated`);
     } catch (e) {
       console.error("failed to parse yaml.");
     }
   }
 
   lock[output] = false;
+  console.log(
+    `[${now()}] ${path.relative(path.join(OUT, ".."), output)} is updated`
+  );
 }
 
 function onUnlink(file) {
@@ -50,6 +68,7 @@ function onUnlink(file) {
 const watcher = chokidar.watch(path.join(ROOT, "lookml"), {
   persistent: true,
 });
+watcher.add(path.join(ROOT, "liquid"));
 
 watcher
   // change
